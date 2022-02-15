8e0648a068b1b1de83b41bab3ee2f7de4f047071
Muzammil <shanikbs6242@gmail.com>
2022-02-15 16:59:29 +0500

    new commit

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 
- mongoose.connect("mongodb://localhost:27017/test").then(()=>{
+ mongoose.connect(process.env.DB_URL).then(()=>{
      console.log("connected")
  }).catch((err)=>{
      console.log(err)
