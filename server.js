7be0926cb886f9230df32a246b916785136def6d
Muzammil <shanikbs6242@gmail.com>
2022-02-15 16:56:56 +0500

    new commit

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+require ("dotenv").config()
 const express =  require("express")
 const app = express();
-const Port = 3000
-
+const Port = process.env.PORT || 5000
+const mongoose = require ("./db")
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+app.set("view engine","ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/",(req,res)=>{
-  res.send("Hello world")
+   res.render("list")
+})
+app.post("/",(req,res)=>{
+    var i = req.body.n;
+    console.log(i)
 })
  
 app.listen(Port,()=>{
