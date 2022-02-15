64d1eef3b3adcc9892f17f9ff5bcf9a473f664ea
Muzammil <shanikbs6242@gmail.com>
2022-02-15 19:16:26 +0500

    new commit

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-require ("dotenv").config()
-const express =  require("express")
+require("dotenv").config()
+const express = require("express")
 const app = express();
 const Port = process.env.PORT || 5000
-const mongoose = require ("./db")
+const mongoose = require("./db")
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended:true}))
-
-app.get("/",(req,res)=>{
-   res.render("list")
+app.use(bodyParser.urlencoded({ extended: true }))
+var i1 = [""]
+app.get("/", (req, res) => {
+    res.render("list", { newLists: i1 })
 })
-app.post("/",(req,res)=>{
+app.post("/", (req, res) => {
     var i = req.body.n;
-    console.log(i)
+    i1.push(i)
+    // console.log(i)
+    res.redirect("/")
 })
- 
-app.listen(Port,()=>{
-    console.log(`App is running on Port ${Port}` )
+
+app.listen(Port, () => {
+    console.log(`App is running on Port ${Port}`)
 })
